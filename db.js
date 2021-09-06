bfcdabbecaf87a339c319a5e4d0564ad32f89b16
David Vazquez <davidvazquez1026@gmail.com>
2021-09-06 11:04:49 -0500

    added forms into registration

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const pool = new Pool({
     user: "postgres",
     host: "localhost",
     database: "users",
-    password: "",
+    password: "masterp",
     port: 5432
 })
 
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const userRoutes = require('./src/user/routes')
+const cors = require('cors')
 
 const app = express();
 const port = 5000;
 
 app.use(express.json())
+
+app.use(cors())
 
 app.get("/", (req, res) => {
     res.send("Bristol home route")
